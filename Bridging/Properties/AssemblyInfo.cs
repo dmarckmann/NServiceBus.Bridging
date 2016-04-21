fc91f284bb04af87177fbadde69fc0926f5f7989
Dries Marckmann <dries.marckmann@gmail.com>
2016-04-21 18:05:29 +0200

    nuspec file

--- Bridging/Properties/AssemblyInfo.cs
+++ Bridging/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bridging")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NServiceBus.Bridging")]
+[assembly: AssemblyDescription("NServiceBus.Bridging")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bridging")]
+[assembly: AssemblyCompany("Roadmap")]
+[assembly: AssemblyProduct("NServiceBus.Bridging")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
